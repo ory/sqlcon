316d2bb89a74878e65f027ce332926c97c1f42ec
arekkas <aeneas@ory.am>
2018-04-21 10:00:48 +0200

    Adds prototype error handler for SQL errors

---
+++ error.go
@@ -0,0 +1,33 @@
+package sqlcon
+
+import (
+	"github.com/go-sql-driver/mysql"
+	"github.com/lib/pq"
+	"github.com/ory/herodot"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+var (
+	ErrUniqueViolation = &herodot.DefaultError{
+		CodeField:   http.StatusBadRequest,
+		StatusField: http.StatusText(http.StatusBadRequest),
+		ErrorField:  "Unable to insert row because a column value is not unique",
+	}
+)
+
+func HandleError(err error) error {
+	if err, ok := err.(*pq.Error); ok {
+		switch err.Code.Name() {
+		case "unique_violation":
+			return errors.Wrap(ErrUniqueViolation, err.Error())
+		}
+		return errors.WithStack(err)
+	}
+
+	if err, ok := err.(*mysql.MySQLError); ok {
+		return errors.WithStack(err)
+	}
+
+	return errors.WithStack(err)
+}
